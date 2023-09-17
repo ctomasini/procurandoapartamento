6ccc7a78e4928c409d0a7873581926ae8c4895b5
Allexz <jallexz@msn.com>
2023-09-17 17:09:18 -0300

    alterado para classe abstrata

--- src/ProcurandoApartamento.Angular/src/services/baseRest.service.ts
+++ src/ProcurandoApartamento.Angular/src/services/baseRest.service.ts
@@ -3,18 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export abstract class BaseRestService {
-  protected actionUrl: string = 'http://localhost:5000/api/';
-  private endPoint: string = '';
+export class BaseRestService {
 
-  constructor(
-    pEndpoint: string
-  ) {
-    this.endPoint = pEndpoint;
-    this.setApiUrl();
-  }
-
-  private setApiUrl(){
-    this.actionUrl = `${this.actionUrl}${this.endPoint}`;
-  }
+  constructor() { }
 }
